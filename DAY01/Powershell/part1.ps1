121d800ae150b02d13dbc3e22e4c116f023ab3ca
yves848 <yves.godart@gmail.com>
2023-12-01 21:40:40 +0100

    code review

--- DAY01/Powershell/part1.ps1
+++ DAY01/Powershell/part1.ps1
@@ -1,4 +1,4 @@
-$lines = (Get-Content data.txt -Raw) -split "\n"
+$lines = (Get-Content ..\data.txt -Raw) -split "\n"
 ($lines | ForEach-Object {
   $r = $_ | Select-String  -Pattern '\d' -AllMatches 
   ($r.Matches.count -eq 1) ? [int]"$($r.Matches[0])$($r.Matches[0])" : [int]"$($r.Matches[0])$($r.Matches[-1])" 
--- DAY01/Powershell/part2.ps1
+++ DAY01/Powershell/part2.ps1
@@ -1,27 +1,22 @@
-$lines = (Get-Content data.txt) -split "\n"
+$lines = (Get-Content ..\data.txt) -split "\n"
+$reg1 = [regex]::new("\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
+$reg2 = [regex]::new("\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\d", "RightToLeft")
 $numbers = @("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 ($lines | ForEach-Object {
   $line = $_
-  $temp = ($numbers -join ')|(')
-  $pattern = "($($temp))"
-  $reg1 = [regex]::new($pattern)
-  $match = $reg1.Match($line)
-  if ($match.Success) {
-    $index = ($numbers.IndexOf($_.Value) + 1)
-    $result = $line.Replace($_.Value, $index)
-    $line = $result
+  $r1 = $reg1.Match($line)
+  $rtn=""
+  if ([int]::TryParse($r1.value,[ref]$rtn)) {
+    $op1=[int]$r1.Value
+  } else {
+    $op1= $numbers.IndexOf($r1.Value) +1
   }
-  $reg2 = [regex]::new($pattern,"RightToLeft")
-  $match = $reg2.Match($line)
-  if ($match.Success) {
-    $index = ($numbers.IndexOf($_.Value) + 1)
-    $result = $line.Replace($_.Value, $index)
-    $line = $result
+  $r2 = $reg2.Match($line)
+  $rtn=""
+  if ([int]::TryParse($r2.value,[ref]$rtn)) {
+    $op2=[int]$r2.Value
+  } else {
+    $op2= $numbers.IndexOf($r2.Value) +1
   }
-  $r = $line | Select-String  -Pattern '(\d)' -AllMatches 
-  if ($r.Matches.Count -gt 0) {
-    ($r.Matches.count -eq 1) ? [int]"$($r.Matches[0])$($r.Matches[0])" : [int]"$($r.Matches[0])$($r.Matches[-1])" 
-  }
-} | Measure-Object -AllStats).Sum
-
-
+  ($op1 * 10) + $op2
+}) | Measure-Object -sum
