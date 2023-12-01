49861e2aac8740c68ea4cb0f4beee393f621d7e6
yves848 <yves.godart@gmail.com>
2023-12-01 23:49:44 +0100

    nettoyage code

--- DAY01/Powershell/part2.ps1
+++ DAY01/Powershell/part2.ps1
@@ -1,22 +1,22 @@
-$lines = (Get-Content ..\data.txt) -split "\n"
-$reg1 = [regex]::new("\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
-$reg2 = [regex]::new("\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\d", "RightToLeft")
-$numbers = @("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-($lines | ForEach-Object {
-  $line = $_
-  $r1 = $reg1.Match($line)
-  $rtn=""
-  if ([int]::TryParse($r1.value,[ref]$rtn)) {
-    $op1=[int]$r1.Value
-  } else {
-    $op1= $numbers.IndexOf($r1.Value) +1
-  }
-  $r2 = $reg2.Match($line)
-  $rtn=""
-  if ([int]::TryParse($r2.value,[ref]$rtn)) {
-    $op2=[int]$r2.Value
-  } else {
-    $op2= $numbers.IndexOf($r2.Value) +1
-  }
-  ($op1 * 10) + $op2
-}) | Measure-Object -sum
+    $lines = (Get-Content ..\data.txt) -split "\n"
+    $reg1 = [regex]::new("\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
+    $reg2 = [regex]::new("\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\d", "RightToLeft")
+    $numbers = @("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    ($lines | ForEach-Object {
+      $line = $_
+      $r1 = $reg1.Match($line)
+      $rtn=""
+      if ([int]::TryParse($r1.value,[ref]$rtn)) {
+        $op1=[int]$r1.Value
+      } else {
+        $op1= $numbers.IndexOf($r1.Value) +1
+      }
+      $r2 = $reg2.Match($line)
+      $rtn=""
+      if ([int]::TryParse($r2.value,[ref]$rtn)) {
+        $op2=[int]$r2.Value
+      } else {
+        $op2= $numbers.IndexOf($r2.Value) +1
+      }
+      ($op1 * 10) + $op2
+    }) | Measure-Object -sum
--- DAY01/Powershell/test.ps1
+++
@@ -1,9 +0,0 @@
-$lines = (Get-Content example2.txt) -split "\n"
-$sum = 0
-$lines | % {
-  $r = $_ | Select-String  -Pattern '\d' -AllMatches 
-  $r.Line
-  $val = ($r.Matches.count -eq 1) ? [int]"$($r.Matches[0])$($r.Matches[0])" : [int]"$($r.Matches[0])$($r.Matches[-1])" 
-  $sum = $sum + $val
-}
-$sum
