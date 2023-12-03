fbc9e492f19ffdf9232732d1c161c3a16d545d1d
yves848 <yves.godart@gmail.com>
2023-12-04 00:02:37 +0100

    DAY 03 - PART 2

--- DAY03/Powershell/part2.ps1
+++ DAY03/Powershell/part2.ps1
@@ -1,20 +1,26 @@
 $lines = (Get-Content -Path ..\data.txt -Raw) -split "\n"
 $r2 = [regex]::new('\*')
 $r = [regex]::new('\d{1,}')
-
+$rr = [regex]::new('\d{1,}','RightToLeft')
 function gauche(
   [string]$line
 ) {
   $num = $r.Matches($line)
-  $result = ($num | Where-Object { $col -in $_.Index..($_.Index + ($_.Length)) }).value
-  return $result
+  foreach($n in $num) {
+    if ($col -in ($n.Index) .. ($n.Index+($n.Length-1))) {
+      return $n.Value
+    }
+  }
 }
 function droite(
   [string]$line
 ) {
-  $num = $r.Matches($line)
-  $result = ($num | Where-Object { $col -in ($_.Index-1)..(($_.Index) + ($_.Length )) }).value
-  return $result
+  $num = $rr.Matches($line)
+  foreach($n in $num) {
+    if ($col -in ($n.Index) .. ($n.Index+($n.Length-1))) {
+      return $n.Value
+    }
+  }
 }
 
 function isAdjacent(
@@ -25,36 +31,36 @@ function isAdjacent(
   $line = $lines[$l]
   $value = gauche -line $line
   if ($value) { $nb += $value }
-  else {
-    $value = droite -line $line
-    if ($value) { $nb += $value }
+  $value = droite -line $line
+  if ($value) { $nb += $value }
+  
+  $line = $lines[$l - 1]
+  $num = $r.Matches($line)
+  foreach($n in $num) {
+    if ($col -in ($n.Index-1) .. ($n.Index+($n.Length))) {
+      $nb += $n.Value
+    }
   }
   
-
-  $line = $lines[$l - 1]
-  $value = gauche -line $line
-  if ($value) { $nb += $value }
-  else {
-    $value = droite -line $line
-    if ($value) { $nb += $value }
-  }
-
   $line = $lines[$l + 1]
-  $value = gauche -line $line
-  if ($value) { $nb += $value }
-  else {
-    $value = droite -line $line
-    if ($value) { $nb += $value }
+  $num = $r.Matches($line)
+  foreach($n in $num) {
+    if ($col -in ($n.Index-1) .. ($n.Index+($n.Length))) {
+      $nb += $n.Value
+    }
   }
   return $nb
 }
 
 $sum = 0
 foreach ($line in $lines) {
-  $i = $lines.IndexOf($line)
   $stars = $r2.Matches($line)
   foreach ($star in $stars) {
     $res = isAdjacent -col $star.Index
-    Write-Host "$($res[0]),$($res[1])" 
+    if ($res.Count -eq 2) {
+      $sum = $sum + ([int]$res[0]*[int]$res[1])
+      "$($res[0]),$($res[1])" | Out-File -FilePath "restult.txt" -Append
+    }
   }
 } 
+$sum
