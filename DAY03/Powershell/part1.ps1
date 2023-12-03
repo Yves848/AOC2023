3432d57fe8c54abbd0f88ac10ecee638c6bc7a9e
yves848 <yves.godart@gmail.com>
2023-12-03 11:10:03 +0100

    DAY 03 - P1 - même ligne

--- DAY03/Powershell/part1.ps1
+++ DAY03/Powershell/part1.ps1
@@ -1,15 +1,33 @@
 $lines = (Get-Content -path ..\example.txt) -split "\n"
-
+$r2 = [regex]::new('[^(\.)|(\d{1,})]')
+$r = [regex]::new('\d{1,}')
 function isAdjacent(
   [int]$line,
-  [int]$col
+  [int]$col,
+  [int]$len
 ) {
-
+  $adj = $false
+  # Tester sur la même ligne
+  foreach($match in $r2.Matches($lines[$line])) {
+    $i = $match.Index
+    if (-not $adj) {
+      $adj = ($i -eq ($col+$len))
+    }
+  }
+  # if (($line -gt 0) -and ($line -lt $lines.Count)) {
+  #   # Tester ligne suivante
+  #   $r2.Matches($lines[$line+1])
+  # }
+  $adj
 }
-$r = [regex]::new('\d{1,}')
+
+
 foreach($line in $lines) {
-  $index = $lines.IndexOf($line)
+  $i = $lines.IndexOf($line)
   $numbers = $r.Matches($line)
-  
-  $index
+  foreach($number in $numbers) {
+    if (isAdjacent -line $i -col $number.Index -len $number.Length) {
+      $number.Value
+    }
+  }
 }
