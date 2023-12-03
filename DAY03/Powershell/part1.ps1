60e6b511b1163d16cfa3f07f410bfa9dfa2695a4
yves848 <yves.godart@gmail.com>
2023-12-03 12:22:30 +0100

    DAY 03 - part 1

--- DAY03/Powershell/part1.ps1
+++ DAY03/Powershell/part1.ps1
@@ -1,4 +1,4 @@
-$lines = (Get-Content -path ..\example.txt) -split "\n"
+$lines = (Get-Content -path ..\data.txt) -split "\n"
 $r2 = [regex]::new('[^(\.)|(\d{1,})]')
 $r = [regex]::new('\d{1,}')
 function isAdjacent(
@@ -13,21 +13,52 @@ function isAdjacent(
     if (-not $adj) {
       $adj = ($i -eq ($col+$len))
     }
+    if (-not $adj) {
+      $adj = ($i -eq ($col -1))
+    } 
   }
-  # if (($line -gt 0) -and ($line -lt $lines.Count)) {
-  #   # Tester ligne suivante
-  #   $r2.Matches($lines[$line+1])
-  # }
+  if (-not $adj) {
+    if (($line -ge 0) -and ($line -lt $lines.Count)) {
+      # tester le ligne au dessus
+      if ($line -gt 0) {
+      foreach($match in $r2.Matches($lines[$line-1])) {
+        $i = $match.Index
+        if (-not $adj) {
+          $start = $col -1
+          $end = ($len + $col)  
+          $adj = ($i -in ($start..$end))
+        }
+        
+      }
+    }
+      # Tester la ligne en dessous
+      if ($line -lt $lines.Count) {
+      foreach($match in $r2.Matches($lines[$line+1])) {
+        $i = $match.Index
+        if (-not $adj) {
+          $start = $col -1
+          $end = ($len + $col)  
+          $adj = ($i -in ($start..$end))
+        }
+        
+      }
+    }
+    }
+  }
   $adj
 }
 
-
+$sum = 0
 foreach($line in $lines) {
   $i = $lines.IndexOf($line)
+  Write-Host "$($i+1)  : " 
   $numbers = $r.Matches($line)
   foreach($number in $numbers) {
     if (isAdjacent -line $i -col $number.Index -len $number.Length) {
-      $number.Value
+       Write-Host "# $($number.Value)"
+       $sum = $sum + [int]$number.Value
     }
   }
 }
+
+Write-Host "Sum : $sum"
