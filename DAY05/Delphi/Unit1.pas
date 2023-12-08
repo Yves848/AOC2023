426017a07863c35f3b4b8e822bd9dcd07f777223
yves848 <yves.godart@gmail.com>
2023-12-08 12:03:27 +0100

    last try

--- DAY05/Delphi/Unit1.pas
+++ DAY05/Delphi/Unit1.pas
@@ -38,7 +38,7 @@ type
     name: string;
     Coords: tlist<tcoord>;
     constructor create(line: string);
-    function Range(i : Integer) : String;
+    function Range(i: Integer): string;
   end;
 
   TForm1 = class(TForm)
@@ -81,7 +81,7 @@ var
   str: string;
   res: tarray<string>;
   i: integer;
-  iSeeds, iMaps, iCoords : Int64;
+  iSeeds, iMaps, iCoords: Int64;
   regEx: TRegEx;
   Match: tMatch;
   Matches: TMatchCollection;
@@ -94,35 +94,12 @@ var
   j2: Int64;
   j: Int64;
   sLine: string;
-  aCoord : tCoord;
+  aCoord: tCoord;
 begin
   Memo3.Clear;
   Memo2.Clear;
   Memo4.Clear;
-  str := Memo1.Lines.Text;
-  res := str.Split([DL]);
-  seedLocation := high(Int64);
-  regEx := TRegEx.create('(\d{1,})\s(\d{1,})');
-  Matches := regEx.Matches(res[0]);
-  pSeeds := tlist<tSeed>.create;
-
-  for Match in Matches do
-  begin
-    if Match.Success then
-      str := Match.Value;
-    pseed := tSeed.create(str);
-    pSeeds.Add(pseed);
-  end;
-
-  i := 1;
-  pmaps := tlist<tmap>.create;
-  while i < length(res) do
-  begin
-    pmaps.Add(tmap.create(res[i]));
-    inc(i);
-  end;
-  // Les maps sont créées .....
-
+  seedLocation := high(int64);
   iSeeds := 0;
   while iSeeds < pSeeds.Count do
     // Parcourir les paires de seeds ......
@@ -135,9 +112,9 @@ begin
     // Pour chaque seed, parcourir les différentes maps pour trouver la location la plus proche.
     while (j < (Seed1 + nbSeeds)) do
     begin
-      if (j mod 1000000 = 0) then
+      if (j mod 10000000 = 0) then
       begin
-        eStep.Text := (format('Start : %u J : %u End : %u', [pSeeds[iSeeds].Start,j,pSeeds[iSeeds].nb]));
+        eStep.Text := (format('Start : %u J : %u End : %u', [pSeeds[iSeeds].Start, j, pSeeds[iSeeds].nb]));
         Application.ProcessMessages;
       end;
       seed2 := J;
@@ -150,13 +127,6 @@ begin
           // commencer le mapping des seeds
           aCoord := pMaps[iMaps].Coords[iCoords];
           j2 := aCoord.index(Seed2);
-          if (pMaps[iMaps].name.Contains('to-loca')) then
-            if (seedLocation > Seed2) then
-            begin
-              SeedLocation := Seed2;
-              Memo4.Lines.Add(seedLocation.tostring());
-              Application.ProcessMessages;
-            end;
           if (j2 <> seed2) then
           begin
             Seed2 := j2;
@@ -164,19 +134,28 @@ begin
           end;
           inc(iCoords);
         end;
+//        sLine := format(' %s : %u', [pMaps[iMaps].name, seed2]);
+//        Memo3.Lines.Add(sLine);
+        if (iMaps = 6) then
+          if (seedLocation > seed2) then
+          begin
+            SeedLocation := seed2;
+            Application.ProcessMessages;
+          end;
         inc(iMaps);
       end;
       inc(j);
     end;
     inc(iSeeds);
   end;
+  Memo4.Lines.Add(seedLocation.tostring());
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
 var
-  SeedEnum : TEnumerator<tSeed>;
-  iMaps : int64;
-  iCoords : Int64;
+  SeedEnum: TEnumerator<tSeed>;
+  iMaps: int64;
+  iCoords: Int64;
 begin
   // Lister les seeds
 
@@ -198,14 +177,14 @@ begin
   iMaps := 0;
   while (iMaps < pMaps.Count) do
   begin
-       Memo3.Lines.Add(pMaps[iMaps].name);
-       iCoords := 0;
-       while (iCoords < pMaps[iMaps].Coords.Count) do
-       begin
-         Memo3.Lines.Add(pMaps[iMaps].Range(iCoords));
-         inc(icoords);
-       end;
-       inc(iMaps);
+    Memo3.Lines.Add(pMaps[iMaps].name);
+    iCoords := 0;
+    while (iCoords < pMaps[iMaps].Coords.Count) do
+    begin
+      Memo3.Lines.Add(pMaps[iMaps].Range(iCoords));
+      inc(icoords);
+    end;
+    inc(iMaps);
   end;
 end;
 
@@ -220,9 +199,9 @@ end;
 
 procedure TForm1.createmaps;
 var
-  i : Integer; //ligne de map.
+  i: Integer; //ligne de map.
 begin
-  if pMaps <> Nil then
+  if pMaps <> nil then
     FreeAndNil(pMaps);
   pMaps := tlist<tmap>.Create;
   I := 1;
@@ -245,6 +224,7 @@ begin
   pSeeds := tlist<tSeed>.Create;
 
   regEx := TRegEx.create('(\d{1,})\s(\d{1,})');
+  //regEx := TRegEx.create('(\d{1,})');
   Matches := regex.Matches(line);
   for Match in Matches do
   begin
@@ -293,14 +273,15 @@ begin
   end;
 end;
 
-function tmap.Range(i: Integer): String;
+function tmap.Range(i: Integer): string;
 begin
-    if ((i > (Coords.Count-1)) or (i < 0)) then
-      result := 'No coordinates found'
-    else
-    begin
-       result := format('Desination : [%u - %u] Source : [%u - %u]', [Coords[i].destination,(Coords[i].destination+Coords[i].L)-1,Coords[i].source,(Coords[i].source+Coords[i].L)-1]);
-    end;
+  if ((i > (Coords.Count - 1)) or (i < 0)) then
+    result := 'No coordinates found'
+  else
+  begin
+    result := format('Desination : [%u - %u] Source : [%u - %u]', [Coords[i].destination, (Coords[i].destination + Coords[i].L) -
+        1, Coords[i].source, (Coords[i].source + Coords[i].L) - 1]);
+  end;
 end;
 
 { tcoord }
@@ -350,9 +331,11 @@ constructor tSeed.create(line: string);
 var
   bornes: tarray<string>;
 begin
-  bornes := line.Split([' ']);
-  Start := strtoint64(bornes[0]);
-  nb := strtoint64(bornes[1]);
+    bornes := line.Split([' ']);
+    Start := strtoint64(bornes[0]);
+    nb := strtoint64(bornes[1]);
+//  Start := strtoint64(Line);
+//  nb := 1;
 end;
 
 function tSeed.enum: string;
@@ -360,11 +343,11 @@ var
   I: Integer;
 begin
   result := format('%u', [Start]);
-  for I := 1 to nb-1 do
+  for I := 1 to nb - 1 do
   begin
-    result := result + format(',%u',[Start+i])
+    result := result + format(',%u', [Start + i])
   end;
-  result := '['+result+']';
+  result := '[' + result + ']';
 end;
 
 function tSeed.range: string;
